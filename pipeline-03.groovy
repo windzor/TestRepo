5d8d96e5e826d005fb84617c9f6ad81f3ad53991
windzor <windzor.urena@gmail.com>
2024-09-23 15:08:09 -0400

    Update pipeline-03.groovy

--- pipeline-03.groovy
+++ pipeline-03.groovy
@@ -21,7 +21,7 @@ pipeline {
         }
         stage("Release") {
             steps {
-                echo "Release stage."
+                echo "Release stage. Update!!"
             }
         }
     }
